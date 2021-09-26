8122b301382124c539bce81357ee89029babf51c
Shubham <shubh.chavhan00@gmail.com>
2021-09-26 21:10:02 +0530

    Created a component to route to different page on clicking a link and displaying name along with image

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+const ItemDetail = ({ match }) => {
+  const [item, setItem] = useState({
+    images: {},
+  });
+
+  useEffect(() => {
+    infos();
+    console.log(match);
+    // eslint-disable-next-line
+  }, []);
+
+  const infos = () => {
+    fetch(
+      `https://fortnite-api.theapinetwork.com/item/get?id=${match.params.id}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data.item);
+        setItem(data.data.item);
+      });
+  };
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <img src={item.images.background} alt="images" />
+    </div>
+  );
+};
+
+export default ItemDetail;
