daf189d3c8fb54e87d91d588e2b9071e77cbfe30
Shubham <shubh.chavhan00@gmail.com>
2021-10-10 13:47:45 +0530

    added className to the Link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   const navStyle = {
     color: "white",
+    textDecoration: "none",
   };
   return (
     <nav>
       <h1>Logo</h1>
       <ul className="nav-links">
-        <Link to="/" style={navStyle}>
+        <Link className="fancy-links" to="/" style={navStyle}>
           <li>Home</li>
         </Link>
-        <Link to="/about" style={navStyle}>
+        <Link className="fancy-links" to="/about" style={navStyle}>
           <li>About</li>
         </Link>
-        <Link to="/profile" style={navStyle}>
+        <Link className="fancy-links" to="/profile" style={navStyle}>
           <li>Profile</li>
         </Link>
       </ul>
